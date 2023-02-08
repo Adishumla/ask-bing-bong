85dfe174d356765060c574d6cf0ace173626b19e
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-08 09:22:49 +0100

    fixed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   );
 }
